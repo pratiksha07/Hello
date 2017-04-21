1d79c6802bc75749ad1e22da742ec5935c0a6c95
pratiksha <pratikshajadhavroxx@gmail.com>
2017-04-21 09:32:29 +0530

    Hello!

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'shotgun'
+
 
 get '/' do
 	 'Hello World!'
@@ -6,3 +8,35 @@ get '/' do
 end
 
 
+get '/greet' do
+	name = params[:name]
+	if name!=nil
+	 return "Hello #{name}"
+	else
+	 return "Invalid Parameters"
+	end
+end
+
+get '/print_params' do
+	params.to_s
+end
+
+get '/fibo' do
+	n = params[:n]
+	m = n.to_i
+	f = fib(n.to_i)
+	return "Fibo = #{f}"
+end
+
+def fib(n)
+	i=2
+	a=0
+	b=1
+	while i<n
+	 c=a+b
+	 a=b
+	 b=c
+	 i=i+1
+	end
+	return c
+end
